73fd185b2a7ad853c127d97b855013b67060d83b
marder <marcin.derucki@gmail.com>
2025-02-15 19:13:43 +0100

    first commit

---
+++ 01_cout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int number1 = 65;
+    float number2 = 223.98;
+
+    cout << number1 << endl;
+    cout << number2;
+
+    return 0;
+}
