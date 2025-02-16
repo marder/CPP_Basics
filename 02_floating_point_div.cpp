a19503bc3d7fa80d207edeb97a11a490bd49d739
marder <marcin.derucki@gmail.com>
2025-02-16 10:28:18 +0100

    16.02.2025

---
+++ 02_floating_point_div.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    double number1 = 85.0;
+    double number2 = 13.0;
+
+    double result = number1 / number2;
+
+    cout << result << endl;
+
+    return 0;
+}
