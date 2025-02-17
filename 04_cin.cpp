d2d017b1a1b73dec95af23b657b988b91e989da6
marder <marcin.derucki@gmail.com>
2025-02-17 20:06:09 +0100

    17.02.2025

---
+++ 04_cin.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int number1;
+    double number2;
+
+    cin >> number1 >> number2;
+
+    cout << number1 << endl;
+    cout << number2 << endl;
+
+    return 0;
+}
