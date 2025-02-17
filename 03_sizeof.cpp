180d665854d31d71d0be46dc76d13c40298d9946
marder <marcin.derucki@gmail.com>
2025-02-17 19:58:47 +0100

    17.02.2025

---
+++ 03_sizeof.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int age;
+    double temperature;
+    char alphabet;
+
+    cout << "char: " << sizeof(char) << " byte" << endl;
+    cout << "int: " << sizeof(int) << " bytes" << endl;
+    cout << "double: " << sizeof(double) << " bytes" << endl;
+
+    return 0;
+}
