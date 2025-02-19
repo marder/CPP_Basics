10d8a38174e5fbba4abb84478b7ebf3f7200bc28
marder <marcin.derucki@gmail.com>
2025-02-19 18:24:50 +0100

    19.02.2025

---
+++ 07_for.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    for (int count = 1; count <= 10; ++count)
+    {
+        cout << "Count = " << count << endl;
+    }
+
+    return 0;
+}
