36189d3ec6c2d738b610b6e65b0d4e4126070c52
marder <marcin.derucki@gmail.com>
2025-02-25 17:47:43 +0100

    25.02.2025

---
+++ 13_function_01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void find_square()
+{
+    int a = 5;
+    int result = a * a;
+    cout << "Square of " << a << " is " << result << endl;
+}
+
+int main()
+{
+
+    find_square();
+
+    return 0;
+}
