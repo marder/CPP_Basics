225a43229384f31779bf2ffa6a5a00d54a5d0e6b
marder <marcin.derucki@gmail.com>
2025-02-27 16:19:45 +0100

    27.02.2025

---
+++ 18_cmath_02.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    double number = 5.0;
+
+    double power = pow(number, 3.0);
+
+    cout << number << " ^ 4 = " << power << endl;
+
+    return 0;
+}
