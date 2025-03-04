9e51eec007e6c16ee71671f4e29d01daeabf44aa
marder <marcin.derucki@gmail.com>
2025-03-04 18:51:46 +0100

    04.03.2025

---
+++ 24_string_02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    string text = "HWDPozytywizm";
+
+    for (int i = 0; i < text.length(); ++i)
+    {
+        cout << text[i];
+    }
+
+    cout << endl;
+
+    return 0;
+}
